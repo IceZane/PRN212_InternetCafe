239f428882aad7dfed972506174d8ec2d52f7e91
IceZane <phuhung258@gmail.com>
2025-11-12 09:29:05 +0700

    giao diện default

---
+++ NetManagement/DAL/Entities/Member.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetManagement;
+
+public partial class Member
+{
+    public int MemberId { get; set; }
+
+    public string AccountName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string CitizenId { get; set; } = null!;
+
+    public decimal? Money { get; set; }
+
+    public DateTime? DateCreated { get; set; }
+
+    public string? Status { get; set; }
+
+    public virtual ICollection<Computer> Computers { get; set; } = new List<Computer>();
+}
---
+++ NetManagement/DAL/Entities/UserAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetManagement;
+
+public partial class UserAccount
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string FullName { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string CitizenId { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public DateTime? DateCreated { get; set; }
+
+    public string? Status { get; set; }
+}
